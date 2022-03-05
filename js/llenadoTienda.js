35325fb489c5d04aec5a47f4f74ad2c6f6a833b4
Juan Camilo David Torres <juanca200043@gmail.com>
2022-03-05 13:31:49 -0500

    se pinta con js

--- js/llenadoTienda.js
+++ js/llenadoTienda.js
@@ -11,17 +11,46 @@ let productosDB = [
     { foto: '../img/imgPagina/Aretes/Arete10.jpg', nombre: "Arete1", precio: 4500, descripcion: "acero laminado" }
 ]
 
+//creo una referencia al componente padre
+//creo una variable para almacenar la base
+//sobre la cual voy a pintar
+
+let fila = document.getElementById("fila")
+
 //Recorrer un arreglo con js
-//Buscar/selecionar/esculcar
 productosDB.forEach(function(producto) {
-    console.log("nacional es el mejor")
+    //paso 1: crear estructura
+    //creo columna
+    let columna = document.createElement("div")
+    columna.classList.add("col")
+
+    //creo la tarjeta
+    let tarjeta = document.createElement("div")
+    tarjeta.classList.add("card")
+    tarjeta.classList.add("h-100")
+
+    //creo la foto
+    let foto = document.createElement("img")
+    foto.classList.add("card-img-top")
+    foto.src = producto.foto
+
+    //paso 2: ordenar la estructura
+    tarjeta.appendChild(foto)
+    columna.appendChild(tarjeta)
+    fila.appendChild(columna)
+
 })
 
-//Escuchando click en el boton
-let boton = document.getElementById("boton")
-boton.addEventListener("click", cambiarFoto)
+//Buscar/selecionar/esculcar
+//productosDB.forEach(function(producto) {
+//console.log("nacional es el mejor")
+//})
 
-function cambiarFoto() {
-    let foto = document.getElementById("fotoPrueba")
-    foto.src = "../img/imgPagina/Aretes/Arete11.jpg"
-}
+//Escuchando click en el boton
+//let boton = document.getElementById("boton")
+//boton.addEventListener("click", cambiarFoto)
+
+//function cambiarFoto() {
+//let foto = document.getElementById("fotoPrueba")
+//foto.src = "../img/imgPagina/Aretes/Arete11.jpg"
+//}
