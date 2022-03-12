f90e338ea2d950a5e7d42f91ff835afa4421f669
Juan Camilo David Torres <juanca200043@gmail.com>
2022-03-12 13:23:28 -0500

    se crean modulos

--- js/llenadoTienda.js
+++ js/llenadoTienda.js
@@ -1,56 +1,84 @@
-let productosDB = [
-    { foto: '../img/imgPagina/Aretes/Arete1.jpg', nombre: "Arete1", precio: 4500, descripcion: "acero laminado" },
-    { foto: '../img/imgPagina/Aretes/Arete2.jpg', nombre: "Arete1", precio: 4500, descripcion: "acero laminado" },
-    { foto: '../img/imgPagina/Aretes/Arete3.jpg', nombre: "Arete1", precio: 4500, descripcion: "acero laminado" },
-    { foto: '../img/imgPagina/Aretes/Arete4.jpg', nombre: "Arete1", precio: 4500, descripcion: "acero laminado" },
-    { foto: '../img/imgPagina/Aretes/Arete5.jpg', nombre: "Arete1", precio: 4500, descripcion: "acero laminado" },
-    { foto: '../img/imgPagina/Aretes/Arete6.jpg', nombre: "Arete1", precio: 4500, descripcion: "acero laminado" },
-    { foto: '../img/imgPagina/Aretes/Arete7.jpg', nombre: "Arete1", precio: 4500, descripcion: "acero laminado" },
-    { foto: '../img/imgPagina/Aretes/Arete8.jpg', nombre: "Arete1", precio: 4500, descripcion: "acero laminado" },
-    { foto: '../img/imgPagina/Aretes/Arete9.jpg', nombre: "Arete1", precio: 4500, descripcion: "acero laminado" },
-    { foto: '../img/imgPagina/Aretes/Arete10.jpg', nombre: "Arete1", precio: 4500, descripcion: "acero laminado" }
-]
+export function pintarTienda() {
 
-//creo una referencia al componente padre
-//creo una variable para almacenar la base
-//sobre la cual voy a pintar
+    let productosDB = [
+        { foto: '../img/imgPagina/Aretes/Arete1.jpg', nombre: "Arete1", precio: 4500, descripcion: "acero laminado" },
+        { foto: '../img/imgPagina/Aretes/Arete2.jpg', nombre: "Arete2", precio: 4500, descripcion: "acero laminado" },
+        { foto: '../img/imgPagina/Aretes/Arete3.jpg', nombre: "Arete3", precio: 4500, descripcion: "acero laminado" },
+        { foto: '../img/imgPagina/Aretes/Arete4.jpg', nombre: "Arete4", precio: 4500, descripcion: "acero laminado" },
+        { foto: '../img/imgPagina/Aretes/Arete5.jpg', nombre: "Arete5", precio: 4500, descripcion: "acero laminado" },
+        { foto: '../img/imgPagina/Aretes/Arete6.jpg', nombre: "Arete6", precio: 5000, descripcion: "acero laminado" },
+        { foto: '../img/imgPagina/Aretes/Arete7.jpg', nombre: "Arete7", precio: 4500, descripcion: "acero laminado" },
+        { foto: '../img/imgPagina/Aretes/Arete8.jpg', nombre: "Arete8", precio: 4500, descripcion: "acero laminado" },
+        { foto: '../img/imgPagina/Aretes/Arete9.jpg', nombre: "Arete9", precio: 4500, descripcion: "acero laminado" },
+        { foto: '../img/imgPagina/Aretes/Arete10.jpg', nombre: "Arete10", precio: 4500, descripcion: "acero laminado" }
+    ]
 
-let fila = document.getElementById("fila")
+    //creo una referencia al componente padre
+    //creo una variable para almacenar la base
+    //sobre la cual voy a pintar
 
-//Recorrer un arreglo con js
-productosDB.forEach(function(producto) {
-    //paso 1: crear estructura
-    //creo columna
-    let columna = document.createElement("div")
-    columna.classList.add("col")
+    let fila = document.getElementById("fila")
 
-    //creo la tarjeta
-    let tarjeta = document.createElement("div")
-    tarjeta.classList.add("card")
-    tarjeta.classList.add("h-100")
+    //Recorrer un arreglo con js
+    productosDB.forEach(function(producto) {
+        //paso 1: crear estructura
+        //creo columna
+        let columna = document.createElement("div")
+        columna.classList.add("col")
 
-    //creo la foto
-    let foto = document.createElement("img")
-    foto.classList.add("card-img-top")
-    foto.src = producto.foto
+        //creo la tarjeta
+        let tarjeta = document.createElement("div")
+        tarjeta.classList.add("card", "h-100")
 
-    //paso 2: ordenar la estructura
-    tarjeta.appendChild(foto)
-    columna.appendChild(tarjeta)
-    fila.appendChild(columna)
+        //creo la foto
+        let foto = document.createElement("img")
+        foto.classList.add("card-img-top")
+        foto.src = producto.foto
 
-})
+        //Nombre del producto
+        let nombreP = document.createElement("h4")
+        nombreP.classList.add("mx-auto")
+        nombreP.textContent = producto.nombre
 
-//Buscar/selecionar/esculcar
-//productosDB.forEach(function(producto) {
-//console.log("nacional es el mejor")
-//})
+        //Precio del producto
+        let precioP = document.createElement("p")
+        precioP.classList.add("mx-auto")
+        precioP.textContent = producto.precio
 
-//Escuchando click en el boton
-//let boton = document.getElementById("boton")
-//boton.addEventListener("click", cambiarFoto)
+        //Descripcion del producto
+        let descripcionP = document.createElement("p")
+        descripcionP.classList.add("mx-auto")
+        descripcionP.textContent = producto.descripcion
 
-//function cambiarFoto() {
-//let foto = document.getElementById("fotoPrueba")
-//foto.src = "../img/imgPagina/Aretes/Arete11.jpg"
-//}
+        //Creando un boton
+        let boton = document.createElement("button")
+        boton.setAttribute("type", "button")
+        boton.classList.add("btn", "btn-info", "mx-4", "my-2")
+        boton.textContent = "Ver producto"
+
+        //paso 2: ordenar la estructura
+        tarjeta.appendChild(foto)
+        columna.appendChild(tarjeta)
+        fila.appendChild(columna)
+        tarjeta.appendChild(nombreP)
+        tarjeta.appendChild(precioP)
+        tarjeta.appendChild(descripcionP)
+        tarjeta.appendChild(boton)
+
+    })
+
+    //Buscar/selecionar/esculcar
+    //productosDB.forEach(function(producto) {
+    //console.log("nacional es el mejor")
+    //})
+
+    //Escuchando click en el boton
+    //let boton = document.getElementById("boton")
+    //boton.addEventListener("click", cambiarFoto)
+
+    //function cambiarFoto() {
+    //let foto = document.getElementById("fotoPrueba")
+    //foto.src = "../img/imgPagina/Aretes/Arete11.jpg"
+    //}
+
+}
